8eeb75fb4f5f3175bf74d3b4f4c0f3646a840b48
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-01-25 13:28:11 -0300

    Types in Ts

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,22 @@ message = "Ronald"
 console.log(message)
 
 message = "Bye World"
+
+/**
+ * types in typescript
+ * number
+ * string
+ * boolean
+ * null
+ * undefined
+ * object
+ * fuction
+ * 
+ * types in typescript
+ * any -> not recommended
+ * unknown 
+ * never
+ * arrays
+ * tuples
+ * enums
+ */
